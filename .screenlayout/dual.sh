662f04475e214757295c917f5c9a67920ce295ab
Xunzhou Ye <yex33@mcmaster.ca>
2025-04-07 13:51:27 -0400

    use high refresh rate

--- .screenlayout/dual.sh
+++ .screenlayout/dual.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --scale 1.75x1.75 --rotate left --output DP-1 --off --output HDMI-0 --primary --mode 3840x2160 --pos 2520x66 --scale 1.78x1.78 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off
+xrandr --output DP-0 --mode 2560x1440 --rate 143.97 --pos 0x0 --scale 1.75x1.75 --rotate left --output DP-1 --off --output HDMI-0 --primary --mode 3840x2160 --pos 2520x66 --scale 1.78x1.78 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off

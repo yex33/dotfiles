cff9df4b894cb35c35ad33d421d1c245ea5b2583
Xunzhou Ye <yex33@mcmaster.ca>
2026-03-26 04:44:41 -0400

    track nvim configs

---
+++ .config/nvim/lua/plugins/extras.lua
@@ -0,0 +1,18 @@
+return {
+  -- Coding
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+
+  -- Languages
+  { import = "lazyvim.plugins.extras.lang.cmake" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.nushell" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.rust" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.toml" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+}

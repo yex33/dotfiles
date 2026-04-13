06337304d7862530c6107fb6eeebc84fb629aff2
Xunzhou Ye <yex33@mcmaster.ca>
2026-04-13 15:52:16 -0400

    lazyvim update and add typos-lsp

---
+++ .config/nvim/lua/plugins/typos.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        typos_lsp = {
+          init_options = {
+            diagnosticSeverity = "Hint",
+          },
+        },
+      },
+    },
+  },
+}

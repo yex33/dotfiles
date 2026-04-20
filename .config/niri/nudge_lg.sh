4bb6505e24202ff077e3611f5d97e65dfd3a1feb
Xunzhou Ye <yex33@mcmaster.ca>
2026-04-20 04:27:44 -0400

    use noctalia theme, update idle resume logic

--- .config/niri/nudge_lg.sh
+++ .config/niri/nudge_lg.sh
@@ -3,7 +3,7 @@ sleep 0.5
 niri msg action power-off-monitors
 sleep 0.5
 niri msg action power-on-monitors
-sleep 0.5
+sleep 2
 niri msg action power-off-monitors
 sleep 0.5
 niri msg action power-on-monitors

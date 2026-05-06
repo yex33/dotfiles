652d6a8bc1e892ae90de24529f9e73da411d8bb5
Xunzhou Ye <yex33@mcmaster.ca>
2026-05-06 04:03:13 -0400

    reorganize files

---
+++ .config/nvim/lua/plugins/autotag.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    opts = {
+      aliases = {
+        ["rust"] = "html",
+      },
+    },
+  },
+}
---
+++ .config/nvim/lua/plugins/rustaceanvim.lua
@@ -0,0 +1,13 @@
+vim.g.rustaceanvim = {
+  server = {
+    settings = {
+      ["rust-analyzer"] = {
+        semanticHighlighting = {
+          strings = {
+            enable = false,
+          },
+        },
+      },
+    },
+  },
+}

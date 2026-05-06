0b4ae44d58bc19b8db3f1ac036b9d916c491339f
Xunzhou Ye <yex33@mcmaster.ca>
2026-05-06 03:43:09 -0400

    treesitter injection for sql in sqlx macro

--- .config/nvim/queries/rust/injections.scm
+++ .config/nvim/queries/rust/injections.scm
@@ -13,3 +13,13 @@
   ]
   (#set! injection.language "html")
 )
+
+(macro_invocation
+  (scoped_identifier
+    path: (identifier) @path (#eq? @path "sqlx")
+    name: (identifier) @name (#eq? @name "query"))
+  (token_tree
+    (raw_string_literal
+        (string_content) @injection.content))
+  (#set! injection.language "sql")
+)

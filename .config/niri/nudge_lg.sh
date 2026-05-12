ae7e5411f067a975a5a4444cec63c3b87dd98a8c
Xunzhou Ye <yex33@mcmaster.ca>
2026-05-12 03:17:26 -0400

    switch from HDMI to DP output

--- .config/niri/nudge_lg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep 0.5
-niri msg action power-off-monitors
-sleep 0.5
-niri msg action power-on-monitors
-sleep 2
-niri msg action power-off-monitors
-sleep 0.5
-niri msg action power-on-monitors
